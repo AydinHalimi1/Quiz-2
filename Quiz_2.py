d9fce924e5a27df552f488fd6c3ac195d5f50c19
Aydin Halimi <aydin_halimi1@baylor.edu>
2022-04-04 15:50:29 -0500

    Quiz 2

---
+++ Quiz_2.py
@@ -0,0 +1,116 @@
+# Identify and update the code below to accomplish the following tasks:
+
+# 1) Rearrange the widgets in the window so that it looks like the picture provided!!!!!!!!!
+# 2) Change the title of the dialog box from 'Quiz II' to 'Joe's Internet Cafe'!!!!!!!!!!!!!!!!!!
+# 3) Change the default option for the radio button to Daytime (when the programs starts up)
+# 4) Fix the Ok and Quit buttons (not currently working)!!!!!!!!!!!!!!!!!!!
+# 5) Add a label and input box (to the mid frame) where the user can enter in the number of minutes spent on the internet!!!!!!!!!
+# 6) The OK button should then display the total cost (properly formatted) of using the internet as per the following scale:
+        # Daytime - 10 cents/minute
+        # Evening - 5 cents/minute
+        # Off-peak - 2 cents/minute
+
+# EXTRA CREDIT
+# Add data validation so that if the OK button is pressed and the input box is empty, the program does not error out.
+
+
+import tkinter
+import tkinter.messagebox
+
+class MyGUI:
+    def __init__(self):
+        # Create the main window.
+        self.main_window = tkinter.Tk()
+        self.main_window.geometry('350x150')
+        self.main_window.title("Joe's Internet Cafe")
+
+
+        self.top_frame = tkinter.Frame(self.main_window)
+        self.mid_frame = tkinter.Frame(self.main_window)
+        self.bottom_frame = tkinter.Frame(self.main_window)
+        
+        
+        self.var = tkinter.IntVar()
+
+        self.var.set(2)
+        
+        #elements for top frame
+        self.rb1 = tkinter.Radiobutton(self.top_frame,
+                                       text='Daytime (6:00 a.m. through 5:59 p.m.)',
+                                       variable=self.var, value=10)
+                                       
+        
+        self.rb2 = tkinter.Radiobutton(self.mid_frame,
+                                       text='Evening (6:00 p.m. through 11:59 p.m.)',
+                                       variable=self.var, value=20)
+
+        
+        self.rb3 = tkinter.Radiobutton(self.mid_frame,
+                                       text='Off-Peak (midnight through 5:59 a.m.)',
+                                       variable=self.var, value=30)
+
+        self.rb1.select()
+        
+        self.rb1.pack()
+        self.rb2.pack()
+        self.rb3.pack()
+
+
+
+        #elements for mid frame (add label and input box here)
+        
+        self.prompt_label = tkinter.Label(self.mid_frame,text='No. of minutes:')
+        
+        self.min_entry = tkinter.Entry(self.mid_frame,width=10)
+
+        self.prompt_label.pack(side='left')
+        self.min_entry.pack(side='left')
+
+
+
+
+
+
+
+        #elements for bottom frame
+        self.ok_button = tkinter.Button(self.bottom_frame,
+                                        text='OK',command=self.calculate)
+        self.quit_button = tkinter.Button(self.bottom_frame,
+                                          text='Quit',command=self.main_window.destroy)
+
+        
+        self.ok_button.pack(side='left')
+        self.quit_button.pack(side='left')
+
+        
+        self.top_frame.pack()
+        self.mid_frame.pack()
+        self.bottom_frame.pack()
+
+        
+       
+        tkinter.mainloop()
+
+
+    
+    def calculate(self):
+        cost = 0
+
+        
+        if self.var.get() == 10:
+            cost += .1
+        if self.var.get() == 20:
+            cost += .05
+        if self.var.get() == 30:
+            cost += .02
+        
+
+#I read the instructions wrong. I messed up the calculation
+      
+
+
+        # Display the message in an info dialog box.
+        tkinter.messagebox.showinfo('Total Cost', '${:,.2f}'.format(cost))
+
+
+my_gui = MyGUI()
---
+++ sgfds.py
@@ -0,0 +1,13 @@
+    def calculate(self):
+        cost1 = .1 * self.var.get()
+        cost2= .05 * self.var.get()
+        cost3 = .02 * self.var.get()
+
+        
+        if self.var.get() == 10:
+            cost1
+        if self.var.get() == 20:
+            cost2
+        if self.var.get() == 30:
+            cost3
+        
